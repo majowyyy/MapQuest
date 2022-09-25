be296e2be5dccd8cd98ab72258fe6e9f531a16d6
majowyyy <marjoriemarcelino011101@gmail.com>
2022-09-25 17:25:09 +0800

    updated mapquest

--- MapQuest.py
+++ MapQuest.py
@@ -1,46 +1,39 @@
 import urllib.parse
 import requests
+from tkinter import *
 
+#Create an instance of tkinter window or frame
+win=Tk()
+win.resizable(False, False)
+win.title("Map Quest with Gas Calculator")
+startloc,destloc,gas = "","",""
 main_api = "https://www.mapquestapi.com/directions/v2/route?"
-key = "LQWxQXpaGxfEEhDZaO3qfNu8fTU5G6o2"
+key = "MhvbvH6lJAKgMu9wisKM5iSoZWOFFPQJ"
+traffic_api = "http://www.mapquestapi.com/search/v2/radius?"
+alternative_api = "http://www.mapquestapi.com/directions/v2/alternateroutes?"
 
-while True:
-    orig = input("Starting Location: ")
-    if orig == "quit" or orig == "q":
-        break
-    dest = input("Destination: ")
-    if dest == "quit" or dest == "q":
-        break
+#function to get the direction from starting location to destination city
+def get_direction():
+        url = main_api + urllib.parse.urlencode({"key":key, "from":startloc, "to":destloc})
+        alt_url = alternative_api + urllib.parse.urlencode({"key":key, "from":startloc, "to":destloc})
+        json_data = requests.get(url).json()
+        json_data_alt = requests.get(alt_url).json()
 
-    url = main_api + urllib.parse.urlencode({"key": key, "from":orig, "to":dest})
-    print("URL: " + (url))
-    json_data = requests.get(url).json()
-    json_status = json_data["info"]["statuscode"]
-    
-    if json_status == 0:
-        print("API Status: " + str(json_status) + " = A successful route call.\n")
-        print("=============================================")
-        print("Directions from " + (orig) + " to " + (dest))
-        print("Trip Duration: " + (json_data["route"]["formattedTime"]))
-        print("Kilometers: " + str("{:.2f}".format((json_data["route"]["distance"])*1.61)))
-        print("Fuel Used (Ltr): " + str("{:.2f}".format((json_data["route"]["fuelUsed"])*3.78)))
-        print("=============================================")
-        for each in json_data["route"]["legs"][0]["maneuvers"]:
-            print((each["narrative"]) + " (" + str("{:.2f}".format((each["distance"])*1.61) + " km)"))
-        print("=============================================\n")
+        json_status = json_data["info"]["statuscode"]
 
-    elif json_status == 402:
-        print("**********************************************")
-        print("Status Code: " + str(json_status) + "; Invalid user inputs for one or both locations.")
-        print("**********************************************\n")
-    elif json_status == 611:
-        print("**********************************************")
-        print("Status Code: " + str(json_status) + "; Missing an entry for one or both locations.")
-        print("**********************************************\n")
-    else:
-        print("************************************************************************")
-        print("For Staus Code: " + str(json_status) + "; Refer to:")
-        print("https://developer.mapquest.com/documentation/directions-api/status-codes")
-        print("************************************************************************\n")
+        # if successfull, display the result
+        if json_status == 0:
 
-        #This is a comment
+                navigate.destroy()
+                Label( win, text="Destination City Location").pack()
+                Label( win, text="Here are the Directions from " + (startloc) + " to " + (destloc)).pack()
+
+                Label( win, text="Total Trip Duration: " + (json_data["route"]["formattedTime"])).pack()
+                Label( win, text="Kilometers: " + str("{:.2f}".format(json_data["route"]["distance"] * 1.6))).pack()
+                Label( win, text="Fuel Used (Ltr): " + str("{:.3f}".format(json_data["route"]["fuelUsed"]*3.78))).pack()
+                Label( win, text="Amount of Money to be Spent on Fuel: " + str("{:.3f}".format(json_data["route"]["fuelUsed"]*3.78 *int(gas)))).pack()
+
+                Label( win, text="Alternative Route: Total Trip Duration: " + (json_data_alt["route"]["formattedTime"])).pack()
+                Label( win, text="Alternative route: Kilometers: " + str("{:.2f}".format(json_data_alt["route"]["distance"] * 1.6))).pack()
+                Label( win, text="Alternative Route: Fuel Used (Ltr): " + str("{:.3f}".format(json_data_alt["route"]["fuelUsed"]*3.78))).pack()
+                Label( win, text="Alternative Route: Amount of Money to be Spent on Fuel: " + str("{:.3f}".format(json_data_alt["route"]["fuelUsed"]*3.78 *int(gas)))).pack()
